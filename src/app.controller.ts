2f8236d5e85c5fe1cd0e5cfc6fd390ac7d10ed1f
vishnuh655 <vishnuh655@gmail.com>
2021-07-07 01:08:36 +0530

    Added API to Validate URL

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
+import { AppService, URLType } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags, ApiQuery } from '@nestjs/swagger';
+import url from 'url';
 
 @Controller()
 @ApiTags('Index')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('validate')
-  @ApiOperation({ summary: 'Get Hello' })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
-  getHello(): string {
-    return this.appService.getHello();
+  /**
+   * API to validate a given URL
+   * @param queryParams url
+   * @returns Object
+   */
+  @Get('validate/:url')
+  @ApiOperation({ summary: 'Validate URL' })
+  @ApiQuery({ name: 'url' })
+  @ApiResponse({ status: 200, description: 'Ok.' })
+  async validateUrl(@Query() queryParams: any): Promise<any> {
+    console.log(queryParams.url);
+
+    const validationResp = await this.appService.validateURL(queryParams.url);
+
+    if (validationResp.urlType === URLType.INVALID) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'This is not a valid URL. Please enter a valid URL',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    } else {
+      return validationResp;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import fetch from 'node-fetch';
+
+interface ValidateURL {
+  urlType: URLType;
+  urlIntegrity: boolean;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  /**
+   * To check the validity and integrity of the fiven URL.
+   * @param url Requested URL
+   * @returns Object
+   */
+  async validateURL(url: string): Promise<ValidateURL> {
+    const requestedURL = new URL(url);
+
+    const hostValidated = this.validateHostName(requestedURL.host);
+    const integrityValidated =
+      hostValidated !== URLType.INVALID
+        ? await this.validateUrlIntegrity(requestedURL.toString())
+        : false;
+
+    return {
+      urlType: hostValidated,
+      urlIntegrity: integrityValidated,
+    };
   }
+
+  /**
+   * To check if the URL belongs to a valid host
+   * @param url Hostname
+   * @returns URLType
+   */
+  validateHostName(url: string) {
+    const Regex = {
+      Reddit: /reddit\.com/i,
+    };
+    try {
+      if (Regex.Reddit.test(url)) {
+        return URLType.REDDIT;
+      } else {
+        return URLType.INVALID;
+      }
+    } catch (error) {
+      if (error.code === 'ERR_INVALID_URL') {
+        return URLType.INVALID;
+      } else {
+        throw new Error(error.code);
+      }
+    }
+  }
+
+  /**
+   * To check the Integrity of the requested URL
+   * @param url Requested URL
+   * @returns boolean
+   */
+  async validateUrlIntegrity(url: string) {
+    const validStatusCodes = [HttpStatus.OK, HttpStatus.CREATED];
+    try {
+      const urlResponse = await fetch(url);
+      if (validStatusCodes.includes(urlResponse.status)) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
+  }
+}
+
+export enum URLType {
+  REDDIT = 'reddit',
+  FACEBOOK = 'facebook',
+  TWITTER = 'twitter',
+  INVALID = 'invalid',
 }
